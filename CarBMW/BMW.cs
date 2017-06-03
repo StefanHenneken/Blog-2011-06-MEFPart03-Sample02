1713c401bc1c8069b1bd812e2646eaa299ba2623
Stefan Henneken <stefan.henneken@live.com>
2017-06-03 23:20:41 +0200

    Add project files.

---
+++ CarBMW/BMW.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using CarContract;
+
+namespace CarBMW
+{
+    [Export(typeof(ICarContract))]
+    public class BMW : ICarContract, IDisposable
+    {
+        private BMW()
+        {
+            Console.WriteLine("BMW constructor.");
+        }
+        public string StartEngine(string name)
+        {
+            return String.Format("{0} starts the BMW.", name);
+        }
+        public void Dispose()
+        {
+            Console.WriteLine("Disposing BMW.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using CarContract;
+
+namespace CarHost
+{
+    class Program
+    {
+        [ImportMany(typeof(ICarContract), RequiredCreationPolicy = CreationPolicy.NonShared)]
+        private IEnumerable<Lazy<ICarContract>> CarParts { get; set; }
+
+        static void Main(string[] args)
+        {
+            new Program().Run();
+        }
+        void Run()
+        {          
+            var catalog = new DirectoryCatalog(".");
+            var container = new CompositionContainer(catalog);
+
+            container.ComposeParts(this);
+            foreach (Lazy<ICarContract> car in CarParts)
+                Console.WriteLine(car.Value.StartEngine("Sebastian"));
+
+            Console.WriteLine("");
+            Console.WriteLine("ReleaseExports.");
+            container.ReleaseExports<ICarContract>(CarParts);
+            Console.WriteLine("");
+
+            container.ComposeParts(this);
+            foreach (Lazy<ICarContract> car in CarParts)
+                Console.WriteLine(car.Value.StartEngine("Sebastian"));
+
+            Console.WriteLine("");
+            Console.WriteLine("ReleaseExports.");
+            foreach (Lazy<ICarContract> car in CarParts)
+                container.ReleaseExport<ICarContract>(car);
+
+            Console.WriteLine("");
+            Console.WriteLine("Dispose Container.");
+            container.Dispose();
+        }
+    }
+}
